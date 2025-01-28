c0d905ded730588d798871e2f9cae4b4bcb5a007
Daichi <a5822108@aoyama.jp>
2025-01-29 00:38:01 +0900

    5: 調整

--- src/app/AddBill.tsx
+++ src/app/AddBill.tsx
@@ -11,13 +11,6 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
 import { DatePicker } from '@/components/DatePicker'
 
 const AddBill = () => {
